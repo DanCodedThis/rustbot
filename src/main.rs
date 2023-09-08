261277df2b83e2a17f98eed4af581f2da33ea21e
BuildTools <unconfigured@null.spigotmc.org>
2023-09-08 14:24:12 +0300

    added any msg suuport

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
  use std::env;
  use serenity::{
      async_trait,
-     model::{channel::Message, gateway::Ready},
+     model::{channel::Message, gateway::Ready, user::User},
      prelude::*,
  };
  
@@ -10,15 +10,21 @@
  const HELP_COMMAND: &str = "!help";
  
  struct Handler;
- 
  #[async_trait]
  impl EventHandler for Handler {
      async fn message(&self, ctx: Context, msg: Message) {
-         if msg.content == HELP_COMMAND {
-             if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
-                 println!("Error sending message: {:?}", why);
-             }
-         }
+        match msg.content.as_str() {
+            HELP_COMMAND => {
+                if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
+            inf => if !msg.author.bot {
+                if let Err(why) = msg.channel_id.say(&ctx.http, inf).await {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
+        }
      }
  
      async fn ready(&self, _: Context, ready: Ready) {
